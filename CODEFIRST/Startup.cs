8a34ee86036fe7549592d5e25f38f21901685945
thinhhoanglinux <hoangquocthinh134@gmail.com>
2017-10-12 11:01:31 +0700

    Add project files.

---
+++ CODEFIRST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CODEFIRST.Startup))]
+namespace CODEFIRST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
